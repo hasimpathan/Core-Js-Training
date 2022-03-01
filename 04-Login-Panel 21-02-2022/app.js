958063dcdb8f7680aa0dd1763429964d87c3f742
hasimpathan <hasim.pathan@neosoftmail.com>
2022-03-01 10:30:25 +0530

    todo login post user

---
+++ 04-Login-Panel 21-02-2022/app.js
@@ -0,0 +1,58 @@
+let employees = {
+  emp_name: "hasimpathan@gmail.com",
+  emp_password: "pass@786",
+};
+
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const submit = document.getElementById("submit");
+
+email.addEventListener("blur", enterEmail);
+password.addEventListener("blur", enterPassword);
+submit.addEventListener("click", submitForm);
+
+function enterEmail() {
+  let emailId = email.value;
+  if (emailId == employees.emp_name) {
+    email.classList.add("is-valid");
+    email.classList.remove("is-invalid");
+  } else {
+    email.classList.add("is-invalid");
+    email.classList.remove("is-valid");
+  }
+}
+
+function enterPassword() {
+  let passwordValue = password.value;
+  if (passwordValue == employees.emp_password) {
+    password.classList.add("is-valid");
+    password.classList.remove("is-invalid");
+  } else {
+    password.classList.add("is-invalid");
+    password.classList.remove("is-valid");
+  }
+}
+
+function submitForm(error) {
+  error.preventDefault();
+  let success = document.getElementById("success");
+  let inCompleteForm = document.getElementById("inCompleteForm");
+  let emailId = email.value;
+  let passwordValue = password.value;
+  if (
+    emailId == employees.emp_name &&
+    passwordValue == employees.emp_password
+  ) {
+    success.classList.add("show");
+    console.log("valid");
+  } else {
+    console.log("Invalid");
+    success.classList.add("d-none");
+    inCompleteForm.classList.remove("d-none");
+    inCompleteForm.classList.add("d-block");
+    inCompleteForm.classList.add("show");
+  }
+  setTimeout(() => {
+    success.classList.remove("show");
+  }, 5000);
+}
---
+++ 05-06-Post-User 25-02-2022/users.js
@@ -0,0 +1,19 @@
+document.getElementById("fetchUserBtn").addEventListener("click", (e) => {
+  const URL = "https://jsonplaceholder.typicode.com/users";
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => {
+      let html = "";
+      data.forEach((element) => {
+        html += ` <tr style="border-bottom: 1px solid #fff">
+              <th scope="row">${element.id}</th>
+              <td>${element.name}</td>
+              <td>${element.email}</td>
+              <td>${element.phone}</td>
+              <td>${element.website}</td>
+           
+            </tr>    `;
+        document.getElementById("tbody").innerHTML = html;
+      });
+    });
+});
